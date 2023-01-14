b9bf8a309906aa123aa7f911a533659e3de67778
Shahid Ahamd <dev.master8522@gmail.com>
2023-01-14 21:20:12 +0500

    data

---
+++ src/globle/Data.js
@@ -0,0 +1,151 @@
+export const filterData = [
+  { name: "Fast food", image: require("../../assets/FastFood.png"), id: "0" },
+  { name: "Burgers", image: require("../../assets/Burger.png"), id: "1" },
+  { name: "Salads", image: require("../../assets/Salad.png"), id: "2" },
+  { name: "Hotdog", image: require("../../assets/HotDog.png"), id: "3" },
+  { name: "Chinese", image: require("../../assets/Chines.png"), id: "4" },
+  { name: "Mexican", image: require("../../assets/maxcian.png"), id: "5" },
+  { name: "Sea food", image: require("../../assets/plattes.png"), id: "6" },
+];
+
+export const restaurantsData = [
+  {
+    restaurantName: "Mc Donalds",
+    farAway: "21.2",
+    businessAddress: "22 Bessie street, Cape Town",
+    images:
+      "https://thecurrent.pk/wp-content/uploads/2022/09/mcdonalds-lahore-based-facility-smk-mojo-222-bhai.jpg",
+    averageReview: 4.9,
+    numberOfReview: 272,
+    coordinates: { lat: -26.1888612, lng: 28.246325 },
+    discount: 10,
+    deliveryTime: 15,
+    collectTime: 5,
+    foodType: "Burgers, Wraps,Milkshakes...",
+    productData: [
+      {
+        name: "Hand cut chips",
+        price: 29.3,
+        image:
+          "https://d1ralsognjng37.cloudfront.net/ae543df5-612c-4634-8f8f-944ba7e4f0b7.jpeg",
+      },
+      {
+        name: "Big Mac",
+        price: 50.8,
+        image:
+          "https://www.gannett-cdn.com/authoring/2016/11/07/NFTU/ghows-LK-47249bbe-b323-473a-89f2-f3f603e35ce1-4fa35a93.jpeg",
+      },
+      {
+        name: "Chicken Burger",
+        price: 70,
+        image:
+          "https://www.chicken.ca/wp-content/uploads/2013/05/Moist-Chicken-Burgers-1180x580.jpg",
+      },
+    ],
+    id: 0,
+  },
+
+  {
+    restaurantName: "KFC",
+    farAway: "12.7",
+    businessAddress: "22 Bessie street, Cape Town",
+    images:
+      "https://www.ravimagazine.com/wp-content/uploads/2015/04/kfc-pakistan.jpg",
+    averageReview: 4.3,
+    numberOfReview: 306,
+    coordinates: { lat: -26.1891648, lng: 28.2441808 },
+    discount: 20,
+    deliveryTime: 30,
+    collectTime: 10,
+    foodType: "Chicken,Chicken wings... ",
+    productData: [
+      {
+        name: "Hand cut chips",
+        price: 29.3,
+        image:
+          "https://d1ralsognjng37.cloudfront.net/ae543df5-612c-4634-8f8f-944ba7e4f0b7.jpeg",
+      },
+      {
+        name: "Big Mac",
+        price: 50.8,
+        image:
+          "https://www.gannett-cdn.com/authoring/2016/11/07/NFTU/ghows-LK-47249bbe-b323-473a-89f2-f3f603e35ce1-4fa35a93.jpeg",
+      },
+      {
+        name: "Chicken Burger",
+        price: 70,
+        image:
+          "https://www.chicken.ca/wp-content/uploads/2013/05/Moist-Chicken-Burgers-1180x580.jpg",
+      },
+    ],
+    id: 1,
+  },
+
+  {
+    restaurantName: "Steers",
+    farAway: "5",
+    businessAddress: " 17 Olivia Rd, Johannesburg",
+    images:
+      "http://triviahappy.com/wp-content/uploads/2014/05/05302014steers.jpg",
+    coordinates: { lat: -26.1886781, lng: 28.244879 },
+    averageReview: 4.9,
+    numberOfReview: 1272,
+    discount: 12,
+    deliveryTime: 25,
+    collectTime: 15,
+    foodType: "Flame grilled beef Burgers",
+    productData: [
+      {
+        name: "Hand cut chips",
+        price: 29.3,
+        image:
+          "https://d1ralsognjng37.cloudfront.net/ae543df5-612c-4634-8f8f-944ba7e4f0b7.jpeg",
+      },
+      {
+        name: "Big Mac",
+        price: 50.8,
+        image:
+          "https://www.gannett-cdn.com/authoring/2016/11/07/NFTU/ghows-LK-47249bbe-b323-473a-89f2-f3f603e35ce1-4fa35a93.jpeg",
+      },
+      {
+        name: "Chicken Burger",
+        price: 70,
+        image:
+          "https://www.chicken.ca/wp-content/uploads/2013/05/Moist-Chicken-Burgers-1180x580.jpg",
+      },
+    ],
+    id: 2,
+  },
+
+  {
+    restaurantName: "Roman Pizza",
+    farAway: "7",
+    businessAddress: " 15 Atlas Rd, Kempton Park",
+    images: "https://promenade.co.za/uploads/Romans-Pizza-1.jpg",
+    averageReview: 4.3,
+    numberOfReview: 700,
+    coordinates: { lat: -26.1845336, lng: 28.2481691 },
+    discount: null,
+    deliveryTime: 20,
+    collectTime: 10,
+    foodType: "Chicken pizza, Vegetarian pizza...",
+    productData: [
+      {
+        name: "Hand cut chips",
+        price: 29.3,
+        image: "https://bukasapics.s3.us-east-2.amazonaws.com/plate5.png",
+      },
+      {
+        name: "Big Mac",
+        price: 50.8,
+        image: "https://bukasapics.s3.us-east-2.amazonaws.com/plate4.png",
+      },
+      {
+        name: "Chicken Burger",
+        price: 70,
+        image: "https://bukasapics.s3.us-east-2.amazonaws.com/plate3.png",
+      },
+    ],
+    id: 3,
+  },
+];
