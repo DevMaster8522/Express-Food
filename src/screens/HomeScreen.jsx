21274bfbead11dca856944768034b762db58c9b0
Shahid Ahamd <dev.master8522@gmail.com>
2023-01-14 21:21:05 +0500

    Home Screen

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,10 +1,173 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  Text,
+  ScrollView,
+  FlatList,
+  Pressable,
+  Image,
+  Dimensions,
+} from "react-native";
 import HomeHeader from "../components/HomeHeader";
+import { useState } from "react";
+import { Colors } from "../globle/Styles";
+import { Icon } from "react-native-elements";
+import { filterData, restaurantsData } from "../globle/Data";
+import { FoodCard } from "../components/FoodCard";
 
 const HomeScreen = () => {
+  const SCREEN_WIDTH = Dimensions.get("window").width;
+  const [delivery, setDelivery] = useState(true);
+  const [indexCheck, setIndexCheck] = useState("0");
   return (
     <View style={styles.container}>
       <HomeHeader />
+      <ScrollView stickyHeaderIndices={[0]} showsVerticalScrollIndicator={true}>
+        <View>
+          <View
+            style={{
+              marginTop: 10,
+              flexDirection: "row",
+              justifyContent: "space-evenly",
+            }}
+          >
+            <TouchableOpacity
+              onPress={() => {
+                setDelivery(true);
+              }}
+            >
+              <View
+                style={{
+                  ...styles.deliveryBtn,
+                  backgroundColor: delivery ? Colors.buttons : Colors.grey5,
+                }}
+              >
+                <Text style={styles.DeliveryText}>{"Delivery"}</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                setDelivery(false);
+              }}
+            >
+              <View
+                style={{
+                  ...styles.deliveryBtn,
+                  backgroundColor: delivery ? Colors.grey5 : Colors.buttons,
+                }}
+              >
+                <Text style={styles.DeliveryText}>{"Pick-Up"}</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={styles.filterView}>
+          <View style={styles.adressView}>
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <Icon
+                type="material-community"
+                name="map-marker"
+                color={Colors.grey1}
+                size={26}
+              />
+              <Text style={{ marginLeft: 5 }}>{"22 gali no 3"}</Text>
+            </View>
+            <View style={styles.clockView}>
+              <Icon
+                type="material-community"
+                name="clock-time-four"
+                color={Colors.grey1}
+                size={26}
+              />
+              <Text style={{ marginLeft: 5 }}>{"Now"}</Text>
+            </View>
+          </View>
+          <View
+            style={{
+              alignItems: "center",
+              marginLeft: 20,
+            }}
+          >
+            <Icon
+              type="material-community"
+              name="tune"
+              color={Colors.grey1}
+              size={26}
+            />
+          </View>
+        </View>
+        <View style={styles.headerTextView}>
+          <Text style={styles.headerText}>{"Categories"}</Text>
+        </View>
+        <View>
+          <FlatList
+            horizontal={true}
+            showsVerticalScrollIndicator={false}
+            data={filterData}
+            keyExtractor={(item) => {
+              item.id;
+            }}
+            extraData={indexCheck}
+            renderItem={({ item, index }) => (
+              <Pressable
+                onPress={() => {
+                  setIndexCheck(item.id);
+                }}
+              >
+                <View
+                  style={
+                    indexCheck === item.id
+                      ? { ...styles.smallCardSelected }
+                      : { ...styles.smallCard }
+                  }
+                >
+                  <Image
+                    style={{ height: 60, width: 60, borderRadius: 30 }}
+                    source={item.image}
+                    resizeMode="contain"
+                  />
+                  <View>
+                    <Text
+                      style={
+                        indexCheck === item.id
+                          ? { ...styles.smallCardTextSelected }
+                          : { ...styles.smallCardText }
+                      }
+                    >
+                      {item.name}
+                    </Text>
+                  </View>
+                </View>
+              </Pressable>
+            )}
+          />
+        </View>
+        <View style={styles.headerTextView}>
+          <Text style={styles.headerText}>{"Free Delivery Now"}</Text>
+        </View>
+        <View>
+          <FlatList
+            style={{ marginBottom: 10, marginTop: 10 }}
+            horizontal={true}
+            data={restaurantsData}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item }) => (
+              <View>
+                <FoodCard
+                  screenWidth={SCREEN_WIDTH * 0.8}
+                  images={item.images}
+                  restaurantName={item.restaurantName}
+                  farAway={item.farAway}
+                  businessAddress={item.businessAddress}
+                  averageReview={item.averageReview}
+                  numberOfReview={item.numberOfReview}
+                />
+              </View>
+            )}
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 };
@@ -14,5 +177,78 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  deliveryBtn: {
+    paddingHorizontal: 20,
+    borderRadius: 15,
+    paddingVertical: 5,
+  },
+  DeliveryText: {
+    marginLeft: 5,
+    fontSize: 16,
+  },
+  filterView: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    marginHorizontal: 10,
+    marginVertical: 10,
+  },
+  clockView: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginLeft: 20,
+    backgroundColor: Colors.cardbackground,
+    borderRadius: 10,
+    paddingHorizontal: 5,
+    marginRight: 10,
+  },
+  adressView: {
+    flexDirection: "row",
+    backgroundColor: Colors.grey5,
+    borderRadius: 15,
+    paddingVertical: 5,
+    paddingHorizontal: 20,
+    marginRight: 10,
+    marginLeft: 20,
+    justifyContent: "space-evenly",
+  },
+  headerTextView: {
+    backgroundColor: Colors.grey5,
+    paddingVertical: 2,
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: Colors.grey2,
+    paddingLeft: 10,
+  },
+  smallCardSelected: {
+    borderRadius: 10,
+    backgroundColor: Colors.buttons,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 5,
+    width: 80,
+    margin: 10,
+    height: 100,
+  },
+  smallCard: {
+    borderRadius: 10,
+    backgroundColor: Colors.grey5,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 5,
+    width: 80,
+    margin: 10,
+    height: 100,
+  },
+  smallCardTextSelected: {
+    fontWeight: "normal",
+    color: Colors.cardbackground,
+  },
+  smallCardText: {
+    fontWeight: "normal",
+    color: Colors.grey2,
   },
 });
