4eecd32f0895d06ae2c3522a86e49b71920babc8
Shahid Ahamd <dev.master8522@gmail.com>
2023-01-15 16:22:48 +0500

    map

---
+++ src/screens/RestaurantMap.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const RestaurantMap = () => {
+  return (
+    <View>
+      <Text>RestaurantMap</Text>
+    </View>
+  );
+};
+
+export default RestaurantMap;
+
+const styles = StyleSheet.create({});
